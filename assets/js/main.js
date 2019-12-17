ec3bab5d17153bbab8a54aad6d40f3085ee5d8a7
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2019-12-17 21:54:35 +0000

    More js and starting to add css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,33 @@
-//Buttons
 $(document).ready(function(){
+    //Button and description - needs to be hidden first??
+    $('#expand').click(function(){
+        $('#description').slideDown(3000);
+    });
+    $('#expand').click(function(){
+        $('#description').slideUp(3000);
+    });
+    $('#expand').click(function(){
+        $('#description').slideToggle(3000);
+    });
+
+    //Input box - catch submission and take value into variable
     $('input').focus(function(){
-        //alert('Focus');
-        $(this).css('background', 'grey');
+        $(this).css('background', 'lightgrey');
     });
     $('input').blur(function(){
-        //alert('Focus');
         $(this).css('background', 'white');
     });
     $('input').keyup(function(e){
         console.log(e.target.value);
     });
+    $('#searchFor').submit(function(e){
+        e.preventDefault();
+        var film = $('input#film').val();
+        console.log(film);
+    });
 });
 
-//trying to request data from API
+//trying to reach out to API to get data
 const APIKEY = '8d72c04338897f51da8771905f89a242';
 const baseURL = 'https://api.themoviedb.org/3/';
 
