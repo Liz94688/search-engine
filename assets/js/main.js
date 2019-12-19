61f9fef3ef83671f3c5e3bb679738f7009c2bbaf
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2019-12-19 23:15:26 +0000

    Get config and render movie

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
+    let configuration = null;
+    getConfiguration().then(result => {
+        configuration = result;
+        console.log(configuration);
+    });
+    
     //Button and description - needs to be hidden first??
     $('#expand').click(function(){
-        $('#description').slideDown(1000);
-    });
-    $('#expand').click(function(){
-        $('#description').slideUp(1000);
-    });
-    $('#expand').click(function(){
-        $('#description').slideToggle(000);
-    });
+        $('.hidden_div').slideDown(2000);
+    })
 
-    //Input box - catch submission and take value into variable
+    //Input box
     $('input').focus(function(){
         $(this).css('background', 'lightgrey');
     });
@@ -20,11 +20,20 @@ $(document).ready(function(){
     $('input').keyup(function(e){
         console.log(e.target.value);
     });
+
+    //Catch submission
     $('#searchFor').submit(function(e){
         e.preventDefault();
         let choice = $('input#film').val();
         console.log(choice);
-        fetchMovie(choice).then((result) => console.log(result));
+        fetchMovie(choice).then((result) => {
+            console.log(result);
+            var container = document.getElementById('data'); 
+            for (let movie of result.results) {
+                console.log(movie);
+                container.innerHTML += renderMovie(movie, configuration);
+            }
+        });
     });
 });
 
@@ -32,18 +41,44 @@ $(document).ready(function(){
 const APIKEY = '8d72c04338897f51da8771905f89a242';
 //const baseURL = 'https://api.themoviedb.org/3/';
 
-let getData = function(){
+const getConfiguration = function(){
     let url = `https://api.themoviedb.org/3/configuration?api_key=${APIKEY}`;
-    fetch(url)
+    return fetch(url)
         .then((result) => {
             return result.json();
         })
     };
 
-function fetchMovie(choice) {
+const fetchMovie = function(choice) {
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${choice}`;
     return fetch(url)
         .then((result) => {
             return result.json();
-    });
+        });
 };
+
+function renderMovie(movie, configuration) {
+    var url = `${configuration.images.base_url}/${configuration.images.poster_sizes[2]}/${movie.poster_path}`
+    const movieCardTpl = `
+        <div class="card" style="width: 18rem;">
+            <img src="${url}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${movie.title}</h5>
+                <p class="card-text">${movie.overview}</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>`;
+    return movieCardTpl;
+}
+
+function renderPagination(result) {
+//     <nav aria-label="Page navigation example">
+//   <ul class="pagination">
+//     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+//     <li class="page-item"><a class="page-link" href="#">1</a></li>
+//     <li class="page-item"><a class="page-link" href="#">2</a></li>
+//     <li class="page-item"><a class="page-link" href="#">3</a></li>
+//     <li class="page-item"><a class="page-link" href="#">Next</a></li>
+//   </ul>
+// </nav>
+}
