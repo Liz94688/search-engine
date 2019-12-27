b83cd7d9cf8049631db8604f332ef2b28c43d542
Liz94688 <elizabeth.stephens@hotmail.co.uk>
2019-12-27 19:45:36 +0000

    Styling the content and cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,10 @@ $(document).ready(function(){
         console.log(configuration);
     });
     
-    //Button and description - needs to be hidden first??
+    //Button and description
     $('#expand').click(function(){
         $('.hidden_div').slideDown(2000);
-    })
+    });
 
     //Input box
     $('input').focus(function(){
@@ -30,18 +30,18 @@ $(document).ready(function(){
             console.log(result);
             var container = document.getElementById('data'); 
             for (let movie of result.results) {
-                console.log(movie);
+                // console.log(movie);
                 container.innerHTML += renderMovie(movie, configuration);
             }
         });
     });
 });
 
-//trying to reach out to API to get data
+//Reach out to API to get data
 const APIKEY = '8d72c04338897f51da8771905f89a242';
 //const baseURL = 'https://api.themoviedb.org/3/';
 
-const getConfiguration = function(){
+function getConfiguration(){
     let url = `https://api.themoviedb.org/3/configuration?api_key=${APIKEY}`;
     return fetch(url)
         .then((result) => {
@@ -49,8 +49,9 @@ const getConfiguration = function(){
         })
     };
 
-const fetchMovie = function(choice) {
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${choice}`;
+function fetchMovie(choice, page) {
+    page = (typeof page !== 'undefined') ? page : 1;
+    let url = `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${choice}&page=${page}`;
     return fetch(url)
         .then((result) => {
             return result.json();
@@ -58,27 +59,35 @@ const fetchMovie = function(choice) {
 };
 
 function renderMovie(movie, configuration) {
-    var url = `${configuration.images.base_url}/${configuration.images.poster_sizes[2]}/${movie.poster_path}`
+    var url = (movie.poster_path !== null) ? 
+        `${configuration.images.base_url}${configuration.images.poster_sizes[2]}${movie.poster_path}`: '';
     const movieCardTpl = `
-        <div class="card" style="width: 18rem;">
+        <div class="card card-style">
             <img src="${url}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${movie.title}</h5>
                 <p class="card-text">${movie.overview}</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>`;
     return movieCardTpl;
 }
 
-function renderPagination(result) {
-//     <nav aria-label="Page navigation example">
-//   <ul class="pagination">
-//     <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-//     <li class="page-item"><a class="page-link" href="#">1</a></li>
-//     <li class="page-item"><a class="page-link" href="#">2</a></li>
-//     <li class="page-item"><a class="page-link" href="#">3</a></li>
-//     <li class="page-item"><a class="page-link" href="#">Next</a></li>
-//   </ul>
-// </nav>
-}
+//function renderPagination(result) {
+//   <nav aria-label="Search results pages">
+//      <ul class="pagination justify-content-center">
+//          <li class="page-item">
+//              <a class="page-link" href="#" aria-label="Previous">
+//                  <span aria-hidden="true">&laquo;</span>
+//              </a>
+//          </li>
+//          <li class="page-item"><a class="page-link" href="#">1</a></li>
+//          <li class="page-item"><a class="page-link" href="#">2</a></li>
+//          <li class="page-item"><a class="page-link" href="#">3</a></li>
+//          <li class="page-item">
+//              <a class="page-link" href="#" aria-label="Previous">
+//                  <span aria-hidden="true">&laquo;</span>
+//              </a>
+//          </li>
+//      </ul>
+//    </nav>
+//}
